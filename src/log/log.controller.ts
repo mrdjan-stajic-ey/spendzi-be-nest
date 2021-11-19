dbce80950f0f9357a73db999f828d9fdb82c2ea0
mrdjan-stajic-ey <mrdjan.stajic@rs.ey.com>
2021-11-19 10:46:57 +0100

    basic logingn for remote debuging

--- src/log/log.controller.ts
+++ src/log/log.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, HttpStatus, Post, Res } from '@nestjs/common';
+import { Response } from 'express';
 import { LogService } from './log.service';
 import { Log } from './schema/log.schema';
 
@@ -9,5 +10,15 @@ export class LogController {
   @Get()
   async findAll(): Promise<Log[]> {
     return this.logService.findAll();
+  }
+
+  @Post()
+  async logEvent(
+    @Body() logBody: { log: string; data: Record<string, any> },
+    @Res() res: Response,
+  ) {
+    console.log(`LOG ${logBody.log}`);
+    console.table(logBody.data);
+    res.status(HttpStatus.OK).send(true);
   }
 }
